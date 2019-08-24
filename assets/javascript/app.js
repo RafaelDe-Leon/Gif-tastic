c957514ec30beaa9248d8a3afc41abb4ef07fa2d
RafaelDe-Leon <Rafael_De_leon88@hotmail.com>
2019-08-23 21:11:17 -0400

    added a bit more js to fix the button loading issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,41 +3,50 @@ var gifArray = ["Naruto", "Dragon Ball Z", "FullMetal Alchemist"];
 
 
 
-document.addEventListener("DOMContentLoaded", function(){
-   
-// target the button id #gif-find and make a AJAX call
-// $(document).ready(function() {
-  $("#find-gif").on("click", function(event) {
-    // stops submit button from trying to submit a form when clicked
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
 
-    var gif = $("#gif-input")
-      .val()
-      .trim();
+    // target the button id #gif-find and make a AJAX call
+    // $(document).ready(function() {
+    $("#find-gif").on("click", function(event) {
+        // stops submit button from trying to submit a form when clicked
+        event.preventDefault();
 
-    // grab the url from giphy
-    var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" +
-      gif +
-      "&api_key=gRAkQP0ZuZM0TiWEqZAOVsIlv71mhBqk&limit=10";
+        var gif = $("#gif-input")
+            .val()
+            .trim();
 
-    // ajax call
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(data) {
-      $("#gif-view").text(data);
-      console.log(data);
+        // grab the url from giphy
+        var queryURL =
+            "https://api.giphy.com/v1/gifs/search?q=" +
+            gif +
+            "&api_key=gRAkQP0ZuZM0TiWEqZAOVsIlv71mhBqk&limit=10";
+
+        // ajax call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response.data);
+
+            var imageURL = images.fixed + height_still;
+            var ratingGrab = response.data.rating;
+
+            for (var i = 0; i < response.data.length; i++) {
+                $("#gif-view").preppend("<img src='" + response.data[i].response.data.imageURL + "'>");
+
+
+            }
+
+        });
+
+        // here we grab the text inside the input box
+        // gifArray.push(gif);
+        // console.log(queryURL);
+
+        // call renderButtons function
+        renderButtons();
     });
-
-    // here we grab the text inside the input box
-    gifArray.push(gif);
-    console.log(queryURL);
-
-    // call renderButtons function
-    renderButtons();
-  });
-  // end of ajax call
+    // end of ajax call
 
 });
 
@@ -46,17 +55,17 @@ function renderButtons() {
     $("#create-button").empty();
 
     for (var i = 0; i < gifArray.length; i++) {
-      // create a button for every gif search
-      var buttonAdd = $("<button>");
-      // add a class called gif
-      buttonAdd.addClass("gif");
-      // adding a data attribute
-      buttonAdd.attr("data-name", gifArray[i]);
-      //providing the initial button text
-      buttonAdd.text(gifArray[i]);
-      $("#create-button").append(buttonAdd);
+        // create a button for every gif search
+        var buttonAdd = $("<button>");
+        // add a class called gif
+        buttonAdd.addClass("gif");
+        // adding a data attribute
+        buttonAdd.attr("data-name", gifArray[i]);
+        //providing the initial button text
+        buttonAdd.text(gifArray[i]);
+        $("#create-button").append(buttonAdd);
     }
-  }
+}
 
 
 // calling the renderButtons function to display the itinial buttons
